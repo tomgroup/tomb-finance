3b3e8d0be43a0b3650ab2874c675b2cd139f2e50
tomgroup <conda0905@gmail.com>
2022-03-04 23:13:33 +0800

    position issue of header

--- src/pages/layout/header/index.styled.tsx
+++ src/pages/layout/header/index.styled.tsx
@@ -13,6 +13,7 @@ export const HeaderContainer = styled.div`
     .bm-menu-wrap {
       display: none;
     }
+    position: fixed;
   }
   /* transition: all 0.85s ease-in-out; */
   position: sticky;
